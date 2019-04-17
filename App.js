b73bf7a0855cf0968dac49d0357de4f3582bf8eb
Vincent <garciajvincent@gmail.com>
2019-04-16 21:00:01 -0700

    Work on React Native Styling

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +8,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bigBlue: {
+    color: 'dodgerblue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+    fontSize: 24,
+  }
 });
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is a basic Text</Text>
+        <Text style={styles.bigBlue}>Dodger Baseball</Text>
+        <Text style={styles.red}>America!</Text>
+      </View>
+    );
+  }
+}
